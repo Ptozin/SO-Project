79ebfc6234c873368771444d74c80c5344f294a6
aroozcozido <up202004293@up.pt>
2022-05-13 14:46:27 +0100

    minor changes

--- src/phrases.c
+++ src/phrases.c
@@ -4,23 +4,27 @@
 #include <ctype.h>
 
 #define BUFFER_SIZE 1024
-
 char buffer[BUFFER_SIZE];
 
 int get_file_lines(int buff_counter)
 {
     int l_counter = 0;
-    for (int i = 0; i < buff_counter; i++)
-        if (buffer[i] == '.' || buffer[i] == '!' || buffer[i] == '?')
+    for (int i = 0; i <= buff_counter; i++)
+        if (buffer[i] == '.' || buffer[i] == '!' || buffer[i] == '?' || buffer[i] == '\0')
             l_counter++;
     return l_counter;
+}
+
+int new_line(char buffer_char) {
+    if(buffer_char == '.' || buffer_char == '!' || buffer_char == '?')
+        return 1;
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
     FILE *stream;
     int buff_counter;
-
     if (argc == 1)
     {
         fprintf(stderr, "usage: phrases [-l] file\n");
@@ -32,19 +36,29 @@ int main(int argc, char *argv[])
         buff_counter = fread(&buffer, sizeof(char), BUFFER_SIZE, stream);
         printf("%d\n", get_file_lines(buff_counter));
     }
-    else
+    stream = fopen(argv[2], "r");
+    buff_counter = fread(&buffer, sizeof(char), BUFFER_SIZE, stream);
+    printf("--------\n");
+    int line = 1;
+    char *ptr = buffer;
+    printf("[%d] ", line);
+
+
+    while (*ptr != '\0')
     {
-        int i, j = 0;
-        stream = fopen(argv[2], "r");
-        buff_counter = fread(&buffer, sizeof(char), BUFFER_SIZE, stream);
-        printf("--------\n");
-        while (buffer[i] != '\0')
-        {
-            printf("%c", buffer[i]);
-            i++;
+        if(*ptr == '\n')
+            *ptr = ' ';
+        printf("%c", *ptr);
+        if(new_line(*ptr)) {
+            line++;
+            printf("\n[%d]", line);
         }
-        printf("--------\n");
+
+        ptr++;
+
     }
+    printf("--------\n");
+    
 
     fclose(stream);
     return EXIT_SUCCESS;
