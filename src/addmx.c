b7d63e96c2adbd4305c9a98bb0ad7c5a0de080a5
aroozcozido <up202004293@up.pt>
2022-05-13 14:51:50 +0100

    go

--- src/addmx.c
+++ src/addmx.c
@@ -34,9 +34,7 @@ int main(int argc, char *argv[]) {
     fscanf(f1, "%dx%d", &line1, &column1);
     // to read line and column of matrix
     fscanf(f2, "%dx%d", &line2, &column2);
-
-    printf("%d %d :: %d %d \n", line1, column1, line2, column2);
- 
+     
     int matrix1[line1][column1];
     int matrix2[line2][column2];
 
@@ -45,10 +43,8 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i < line1; i++){
         for(int j = 0; j < column1; j++){
             fscanf(f1, "%d ", &matrix1[i][j]);
-            printf("%d ", matrix1[i][j]);
         }
         fscanf(f1, "\n");
-        printf("\n");
     }
 
     rewind(f2);
