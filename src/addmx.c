800d21a807eb5395413e2b62e3a291a84b1d1ef5
Ptozin <up202004293@up.pt>
2022-05-23 16:42:20 +0100

    ex2 mostly done

--- src/addmx.c
+++ src/addmx.c
@@ -19,31 +19,37 @@ int main(int argc, char *argv[]) {
     FILE * f1 = fopen(argv[1], "r");
     if(!f1) {
         printf("file %s could not be read\n", argv[1]);
+        return EXIT_FAILURE;
     }
     FILE * f2 = fopen(argv[2], "r");
     if(!f2) {
         fclose(f1);
         printf("file %s could not be read\n", argv[2]);
+        return EXIT_FAILURE;
     }
 
     char buffer[LINESIZE];
 
     int line1, line2, column1, column2;
 
-    // to read line and column of matrix
+    // to read line and column of matrix1
     fscanf(f1, "%dx%d", &line1, &column1);
-    // to read line and column of matrix
+    // to read line and column of matrix2
     fscanf(f2, "%dx%d", &line2, &column2);
 
     if(line1 != line2 || column1 != column2){
-         printf("Matrices have different sizes");
-         return EXIT_FAILURE;
+        printf("Matrices have different sizes\n");
+        fclose(f1);
+        fclose(f2);
+        return EXIT_FAILURE;
     }
 
     int *ptr = mmap(NULL, 3*line1*column1*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0 );
     if(ptr == MAP_FAILED){
-        printf("Mapping Failed\n");
-        return 1;
+        perror("Mapping Failed. Check if the first line of the file is of type \"n\"x\"m\" ");
+        fclose(f1);
+        fclose(f2);
+        return EXIT_FAILURE;
     }
 
     
@@ -52,7 +58,24 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i < line1; i++){
         for(int j = 0; j < column1; j++){
             if(!fscanf(f1, "%d", &ptr[i*column1+j])){
-                perror("Error while loading matrix1. Check if number of lines and columns correspond with the first line.");
+                printf("Error while loading matrix1. Check if number of lines and columns correspond with the first line.\n");
+                fclose(f1);
+                fclose(f2);
+                return EXIT_FAILURE;
+            }
+            if(i == line1-1 && j == column1-1) {
+                if(!feof(f1)) {
+                    printf("Error while loading matrix1. Make sure there are no newlines at the end of the file.\nCheck if number of lines and columns correspond with the first line.\n");
+                    fclose(f1);
+                    fclose(f2);
+                    return EXIT_FAILURE;
+                }
+            }
+            else if(feof(f1)) {
+                printf("Error while loading matrix1. Check if number of lines and columns correspond with the first line.\n");
+                fclose(f1);
+                fclose(f2);
+                return EXIT_FAILURE;
             }
         }
         fscanf(f1, "\n");
@@ -60,10 +83,27 @@ int main(int argc, char *argv[]) {
 
     rewind(f2);
     fgets(buffer, LINESIZE, f2);
-    for(int i = 0; i < line2; i++){
-        for(int j = 0; j < column2; j++){
-            if(!fscanf(f2, "%d ", &ptr[line1*column1 + i*column2+j])){
-                perror("Error while loading matrix2. Check if number of lines and columns correspond with the first line.");
+    for(int i = 0; i < line1; i++){
+        for(int j = 0; j < column1; j++){
+            if(!fscanf(f2, "%d", &ptr[line1*column1 + i*column2+j])){
+                printf("Error while loading matrix2. Check if number of lines and columns correspond with the first line.\n");
+                fclose(f1);
+                fclose(f2);
+                return EXIT_FAILURE;
+            }
+            if(i == line1-1 && j == column1-1) {
+                if(!feof(f2)) {
+                    printf("Error while loading matrix2. Make sure there are no newlines at the end of the file.\nCheck if number of lines and columns correspond with the first line.\n");
+                    fclose(f1);
+                    fclose(f2);
+                    return EXIT_FAILURE;
+                }
+            }
+            else if(feof(f2)) {
+                printf("Error while loading matrix2. Check if number of lines and columns correspond with the first line.\n");
+                fclose(f1);
+                fclose(f2);
+                return EXIT_FAILURE;
             }
         }
         fscanf(f2, "\n");
@@ -74,6 +114,8 @@ int main(int argc, char *argv[]) {
         pid_t pid;
         if ((pid = fork()) == -1) {
             perror("Fork Error");
+            fclose(f1);
+            fclose(f2);
             return EXIT_FAILURE;
         }
         else if (pid == 0) {
@@ -81,7 +123,6 @@ int main(int argc, char *argv[]) {
             for(int j = 1; j <= line1*column1; j++){
                 if(j % column1 == i || (i == column1 && j % column1 == 0)){
                     *(ptr + (j + (line1 * column1) * 2) - 1) = *(ptr + (j + (line1 * column1)) - 1) + *(ptr + j - 1);
-                    //ptr[j + (line1 * column1) * 2] = ptr[j * (line1 * column1)] + ptr[j];
                 }
             }
             return EXIT_SUCCESS;
@@ -89,7 +130,9 @@ int main(int argc, char *argv[]) {
         else {
             /* parent process */
             if (waitpid(pid, NULL, 0) == -1) {
-                perror("Wait Error");
+                perror("Wait Error\n");
+                fclose(f1);
+                fclose(f2);
                 return EXIT_FAILURE;
             }
         }
@@ -107,16 +150,3 @@ int main(int argc, char *argv[]) {
     fclose(f2);
     return EXIT_SUCCESS;
 }
-
-/*
-sacar n = argv[1][0], m = argv[1][2]
-fazer for(i < n)
-    fork()
-    processo filho:
-    for(j < m)
-        matriz1[i] + matriz2[i]
-        guardar resultado
-
-    matar o filho
-dar print 
-*/
